1b87704469543183fac6163e71152a32715ad127
Yiwang5 <yiwang5@s.sfusd.edu>
2025-04-28 15:47:30 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -8,7 +8,7 @@ public class CheckDigit
    public static int getCheck(int num) 
    {  
      int sum = 0;
-     for(int i = 1; I < getNumberOfDigits(num); i++){
+     for(int i = 1; i < getNumberOfDigits(num); i++){
      sum =  sum + ((8 - i) * getDigit(num, i));
      
      }
